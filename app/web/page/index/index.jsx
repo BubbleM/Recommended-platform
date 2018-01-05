ebf02c2b7d93852d73d71430e9a0d25ddf50e319
liumin15 <liumin15@58ganji.com>
2018-01-05 17:02:16 +0800

    添加图表配置

--- app/web/page/index/index.jsx
+++ app/web/page/index/index.jsx
@@ -4,18 +4,16 @@ import fetchJsonp from 'fetch-jsonp';
 // 引入 ECharts 主模块
 import Echarts from 'echarts';
 // 引入柱状图
-import  'echarts/lib/chart/bar';
+import 'echarts/lib/chart/bar';
 // 引入提示框和标题组件
 import 'echarts/lib/component/tooltip';
 import 'echarts/lib/component/title';
 import './index.css';
 
 export default class Home extends Component {
-  componentDidMount() {
-        // 基于准备好的dom，初始化echarts实例
-        var myChart = Echarts.init(document.getElementById('main'));
-        // 绘制图表
-        myChart.setOption({
+    constructor(props) {
+        super(props);
+        let options = {
             title: {
                 text: 'ECharts 入门示例'
             },
@@ -26,43 +24,43 @@ export default class Home extends Component {
             yAxis: {},
             series: [{
                 name: '销量',
-                type: 'bar',
+                type: 'line',
                 data: [5, 20, 36, 10, 10, 20]
             }]
-        });
+        };
+        this.state = { option: options };
+    }
 
-        // const url = "/ajax?action=getchexingconf&source=car&value="+value+"&cateid="+cateid        
+    componentDidMount() {
         fetchJsonp('http://127.0.0.1:8000/hello')
         .then(function(response) { return response.json();})
         .then(res => {
             console.log(res);
+        });
+        var myChart = Echarts.init(document.getElementById('main'));
+        myChart.setOption(this.state.option);
+    }
+
+    render() {
+        let self = this;
+        return <div>
+            <Header></Header>
+            <div id='main' className="mains"></div>
+            <div className='gadeEchart'>
+                <button onClick={self.chooseChart.bind(self,'line')}>折线图</button>
+                <button onClick={self.chooseChart.bind(self,'bar')}>条形图</button>
+                <button onClick={self.chooseChart.bind(self,'pie')}>饼状图</button>
+            </div>
+        </div>;
+    }
+
+    chooseChart(type){
+        let self = this;
+        var myChart = Echarts.init(document.getElementById('main'));
+        let { option } = self.state;
+        option.series[0].type = type;
+        self.setState({option: option},function(){
+            myChart.setOption(option);
         })
-
-        /* const xhr = new XMLHttpRequest();
-        xhr.open('GET', 'http://127.0.0.1:8000/hello', true);
-        xhr.withCredentials = true;
-        xhr.send();
-        xhr.onreadystatechange = () =>{
-            if(xhr.readyState == XMLHttpRequest.DONE){
-                if(xhr.status == 200){
-                    console.log(xhr.responseText);
-                    let gotServices = JSON.parse(xhr.responseText);
-                }
-            }else{
-                alert('ajax失败了');
-            }
-        } */
-  }
-
-  render() {
-    return <div>
-        <Header></Header>
-        <div id='main' className="mains"></div>
-        <div id='gadeEchart'>
-            <button>折线图</button>
-            <button>条形图</button>
-            <button>饼状图</button>
-        </div>
-    </div>;
-  }
+    }
 }
