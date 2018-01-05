0d27bc6b7aceaa2b7b0cfe48b2d7fe7214ed9649
liumin15 <liumin15@58ganji.com>
2018-01-05 19:52:58 +0800

    处理数据接口

--- app/web/page/index/index.jsx
+++ app/web/page/index/index.jsx
@@ -15,7 +15,7 @@ export default class Home extends Component {
         super(props);
         let options = {
             title: {
-                text: 'ECharts 入门示例'
+                text: '推荐结果展示'
             },
             tooltip: {},
             xAxis: {
@@ -23,29 +23,62 @@ export default class Home extends Component {
             },
             yAxis: {},
             series: [{
-                name: '销量',
+                name: '相关程度',
                 type: 'line',
                 data: [5, 20, 36, 10, 10, 20]
             }]
         };
         this.state = { option: options };
+        this.formatData = this.formatData.bind(this);
+    }
+
+    formatData(res) {
+        let self = this, xData = [], yData = [];
+        let { option } = self.state;
+
+        res.forEach(item => {
+            xData.push(item[0]);
+            yData.push(item[1]);
+        });
+        option.xAxis.data = xData;
+        option.series[0].data = yData;
+        self.setState({option: option},function(){
+            var myChart = Echarts.init(document.getElementById('main'));
+            myChart.setOption(option);
+        });
     }
 
     componentDidMount() {
-        fetchJsonp('http://127.0.0.1:8000/hello')
+        let users = {'Angelica': {'Blues Traveler': 3.5, 'Broken Bells': 2.0, 'Norah Jones': 4.5, 'Phoenix': 5.0,
+        'Slightly Stoopid': 1.5, 'The Strokes': 2.5, 'Vampire Weekend': 2.0},
+'Bill':     {'Blues Traveler': 2.0, 'Broken Bells': 3.5, 'Deadmau5': 4.0, 'Phoenix': 2.0,
+        'Slightly Stoopid': 3.5, 'Vampire Weekend': 3.0},
+'Chan':     {'Blues Traveler': 5.0, 'Broken Bells': 1.0, 'Deadmau5': 1.0, 'Norah Jones': 3.0,
+         'Phoenix': 5, 'Slightly Stoopid': 1.0},
+'Dan':      {'Blues Traveler': 3.0, 'Broken Bells': 4.0, 'Deadmau5': 4.5, 'Phoenix': 3.0,
+        'Slightly Stoopid': 4.5, 'The Strokes': 4.0, 'Vampire Weekend': 2.0},
+'Hailey':   {'Broken Bells': 4.0, 'Deadmau5': 1.0, 'Norah Jones': 4.0, 'The Strokes': 4.0,
+         'Vampire Weekend': 1.0},
+'Jordyn':   {'Broken Bells': 4.5, 'Deadmau5': 4.0, 'Norah Jones': 5.0, 'Phoenix': 5.0,
+        'Slightly Stoopid': 4.5, 'The Strokes': 4.0, 'Vampire Weekend': 4.0},
+'Sam':      {'Blues Traveler': 5.0, 'Broken Bells': 2.0, 'Norah Jones': 3.0, 'Phoenix': 5.0,
+         'Slightly Stoopid': 4.0, 'The Strokes': 5.0},
+'Veronica': {'Blues Traveler': 3.0, 'Norah Jones': 5.0, 'Phoenix': 4.0, 'Slightly Stoopid': 2.5,
+         'The Strokes': 3.0}
+}
+
+        fetchJsonp('http://127.0.0.1:8000/hello?data='+JSON.stringify(users))
         .then(function(response) { return response.json();})
         .then(res => {
-            console.log(res);
+            this.formatData(res);
         });
-        var myChart = Echarts.init(document.getElementById('main'));
-        myChart.setOption(this.state.option);
     }
 
     render() {
         let self = this;
         return <div>
             <Header></Header>
-            <div id='main' className="mains"></div>
+            <div id='main' className='mains'></div>
             <div className='gadeEchart'>
                 <button onClick={self.chooseChart.bind(self,'line')}>折线图</button>
                 <button onClick={self.chooseChart.bind(self,'bar')}>条形图</button>
