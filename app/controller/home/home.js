e54f792e5f4afef53f4858999456b10dbb6f2c90
liumin15 <liumin15@58ganji.com>
2018-01-30 17:31:07 +0800

    添加业务接口

--- app/controller/home/home.js
+++ app/controller/home/home.js
@@ -1,27 +1,61 @@
 const Model = require('../../mocks/article/list');
 module.exports = app => {
   return class AppController extends app.Controller {
-    async index() {
+    async indexPage() {
       const { ctx } = this;
-      // await ctx.render('home/home.js', Model.getPage(1, 2));
-      await ctx.render('index/index.js')
+      await ctx.render('home/home.js', Model.getPage(1, 10));
     }
-
-    async home(){
-      const { ctx } = this;
-      await ctx.render('home/home.js', Model.getPage(1, 2));
-    }
-
-    async login(){
+    async loginPage(){
       const { ctx } = this;
       await ctx.render('login/login.js');
     }
-
-    async register(){
+    async registerPage(){
       const { ctx } = this;
       await ctx.render('register/register.js');
     }
+    async homePage(){
+      const { ctx } = this;
+      if (ctx.cookies.get('username')) {
+        await ctx.render('index/index.js');
+      } else {
+        await ctx.render('login/login.js', {info: '请登陆后查看'});
+      }    
+    }
+    async showPage(){
+      const { ctx } = this;
+      if (ctx.cookies.get('username')) {
+        await ctx.render('show/show.js'); 
+      } else {
+        await ctx.render('login/login.js', {info: '请登陆后查看'});
+      }     
+    }
 
+    async register(){ // 平台用户注册
+      const { ctx } = this;
+      const data = this.ctx.query;
+      let result = await this.app.mysql.insert('user', {username: data.username, phone: data.phone, pwd: data.pwd});
+      if (result.affectedRows == 1) { 
+        ctx.redirect('/');
+      } else {
+        // 注册失败
+      }
+    }
+    async submit(){ // 提交数据
+      const { ctx } = this;
+      const query = this.ctx.query.data;
+      await this.app.mysql.insert('info', {attr: query, uid:1});
+    }
+    async login(){ // 平台用户登陆
+      const { ctx } = this;
+      const data = this.ctx.query;
+      let result = await this.app.mysql.query('select pwd from user where phone = ?', [data.phone]);
+      if (result[0].pwd === data.pwd) {
+        ctx.cookies.set('username', data.phone, {maxAge: 1000*60*60*24});
+        ctx.redirect('/');
+      } else {
+        // 登陆失败
+      }
+    }
    /*  async client() {
       const { ctx } = this;
       await ctx.renderClient('home/home.js', Model.getPage(1, 10));
--- app/router.js
+++ app/router.js
@@ -1,7 +1,12 @@
 
 module.exports = app => {
-  app.get('/', app.controller.home.home.index);
-  app.get('/home', app.controller.home.home.home);
-  app.get('/login', app.controller.home.home.login);
+  app.get('/', app.controller.home.home.indexPage);
+  app.get('/homePage', app.controller.home.home.homePage);
+  app.get('/loginPage', app.controller.home.home.loginPage);
+  app.get('/registerPage', app.controller.home.home.registerPage);
+  app.get('/showPage', app.controller.home.home.showPage);
+
+  app.get('/submit', app.controller.home.home.submit);
   app.get('/register', app.controller.home.home.register);
+  app.get('/login', app.controller.home.home.login);
 };
