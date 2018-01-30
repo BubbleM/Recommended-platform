59b4db4cc3a348593ca131987930c4cb63d9546c
liumin15 <liumin15@58ganji.com>
2018-01-30 17:34:07 +0800

    添加MySQL连接配置

--- config/config.local.js
+++ config/config.local.js
@@ -1,4 +1,3 @@
-
 const ip = require('ip');
 const EasyWebpack = require('easywebpack-react');
 module.exports = () => {
@@ -30,6 +29,16 @@ module.exports = () => {
   });
 
   exports.security = { domainWhiteList };
-
+  exports.mysql = {
+    client: {
+      host: '127.0.0.1',
+      port: '3306',
+      user: 'root',
+      password: '123456',
+      database: 'datacollection',
+    },
+    app: true,
+    agent: false,
+  };
   return exports;
 };
