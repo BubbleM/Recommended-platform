f1ca05802cd1c9a8b048c6693e91bb5b53fba77c
BubbleM <2436347334@qq.com>
2018-04-01 21:14:59 +0800

    添加首页面footer

--- app/web/page/home/home.jsx
+++ app/web/page/home/home.jsx
@@ -3,15 +3,15 @@ import Header from 'component/header/header.jsx';
 // import List from 'component/home/list.jsx';
 import ProdBanner from 'component/banner/prodbanner.jsx';
 import AttrBanner from 'component/banner/attrbanner.jsx';
-import './home.css';
+import './home.sass';
 export default class Home extends Component {
   componentDidMount() {
   }
 
   render() {
-    return <div>
+    return <div className="home_wrap">
       <Header></Header>
-      <div className="main">
+      <div className="content">
         <ProdBanner data={this.props.bgInfo[0]}/>
         <AttrBanner data={this.props.attrInfo}/>
         <ProdBanner data={this.props.bgInfo[1]}/>
@@ -20,6 +20,17 @@ export default class Home extends Component {
           <List list={this.props.list}></List>
         </div> */}
       </div>
+      <footer>
+        <span>Amazing基于Node.js和React的数据推荐平台©2018展示专用</span><br/><br/>
+        <span>
+          <a href="/registerPage">注册&nbsp;|&nbsp;</a>
+          <a href="/loginPage">登录&nbsp;|&nbsp;</a>
+          <a href="">关于我们&nbsp;|&nbsp;</a>
+          <a href="">功能&nbsp;|&nbsp;</a>
+          <a href="">隐私权政策&nbsp;|&nbsp;</a>
+          <a href="">服务条款</a>
+        </span>
+      </footer>
     </div>;
   }
 }
