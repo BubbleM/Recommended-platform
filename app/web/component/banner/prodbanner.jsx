4afe45381e1aca8b2c5a959798cf1beb72ba8ad8
BubbleM <2436347334@qq.com>
2018-04-01 20:57:11 +0800

    添加首页面大背景展示组件

---
+++ app/web/component/banner/prodbanner.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './prodbanner.sass';
+
+export default class ProdBanner extends Component{
+  render(){
+    let self = this;
+    let { data } = self.props;
+    return(
+      <div className="prodbanner_wrap row">
+        <img src={data.img} alt=""/>
+        <div className="intro-holder col-md-5">
+          <h2>{data.title}</h2>
+          <p>{data.msg}</p>
+        </div>
+      </div>
+    )
+  }
+  componentDidMount(){}
+}
