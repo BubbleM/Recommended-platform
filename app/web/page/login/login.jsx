ef33a1c66d009077e4d682d9a855c0e81dcde3b5
BubbleM <2436347334@qq.com>
2018-04-03 20:52:47 +0800

    优化登录页面 添加动态背景完善登录表单

--- app/web/page/login/login.jsx
+++ app/web/page/login/login.jsx
@@ -5,17 +5,41 @@ import './login.sass';
 export default class Home extends Component {
   render() {
     return <div>
-      {/* <Header></Header> */}
-      <canvas id="myCanvas"></canvas>
-      <div className="main">
-        {this.props.info ? this.props.info : '登陆'}
+      <Header></Header>
+      <div className="sign_container">
+        <div className="sign_header">
+          {/* <img src="/public/assets/LOGO" alt="logo"/> */}
+          <div className="title">个性化数据推荐平台</div>
+        </div>
         <form action="/login" method="GET">
-          <p>手机号: <input type="text" placeholder="请输入手机号" name="phone"/></p>
-          {/* <p>昵称: <input type="text" placeholder="请输入用户名" name="username"/></p> */}
-          <p>密码: <input type="password" placeholder="请输入密码" name="pwd"/></p>
-          <input type="submit" value="Submit" />
+          <div className="form-group">
+            <div className="input-group">
+              <div className="input-group-addon">
+                <i className="glyphicon glyphicon-earphone"></i>
+              </div>
+              <input type="text" className="form-control" name="phone" placeholder="请输入手机号"/>
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="input-group">
+              <div className="input-group-addon">
+                <i className="glyphicon glyphicon-lock"></i>
+              </div>
+              <input type="password" className="form-control" name="pwd" placeholder="请输入密码"/>
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="input-group">&nbsp;
+				      <input type="checkbox" id="c1" name="remember"/>
+						  <label for="c1">&nbsp;&nbsp;记住我</label>
+				    </div>
+          </div>
+          <div className="form-group">
+					  <button type="submit" className="btn btn-primary width100">登录</button>
+				  </div>
         </form>
       </div>
+      <canvas id="myCanvas"></canvas>
     </div>;
   }
   componentDidMount() {
