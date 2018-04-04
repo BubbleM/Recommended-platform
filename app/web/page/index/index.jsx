ab3d516077c10fb792d2e5cdee40a236962324ed
BubbleM <2436347334@qq.com>
2018-04-04 14:07:03 +0800

    优化完善数据上传页面布局展示

--- app/web/page/index/index.jsx
+++ app/web/page/index/index.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Header from 'component/header/header.jsx';
 import fetchJsonp from 'fetch-jsonp';
-import './index.css';
+import './index.sass';
 // 代码格式化
 import jsBeautify from 'js-beautify/js/lib/beautify'
 let ace // 代码上传区域编辑器样式
@@ -15,51 +15,58 @@ if (typeof window !== 'undefined') {
 }
 
 export default class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.submitData = this.submitData.bind(this);
-        this.getFormatData = this.getFormatData.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.submitData = this.submitData.bind(this);
+    this.getFormatData = this.getFormatData.bind(this);
+  }
 
-    componentDidMount() {
-        let editor = ace.edit(this.refs.codeEditor);
-        editor.getSession().setMode('ace/mode/javascript');
-        editor.setTheme('ace/theme/monokai');
-    }
+  componentDidMount() {
+    let editor = ace.edit(this.refs.codeEditor);
+    editor.getSession().setMode('ace/mode/javascript');
+    editor.setTheme('ace/theme/monokai');
+  }
 
-    render() {
-        let self = this;
-        return <div>
-            <Header></Header>
-            <div className='editPanel'>
-                <div ref="codeEditor" className='codeEditor'></div>
-                <div className='panel-info'>
-                    <button className='editBtn' onClick={this.submitData}>提交数据</button>
-                    <div></div>
-                    <button className='editBtn' onClick={this.getFormatData}>格式化数据</button>
-                </div>
+  render() {
+    let self = this;
+    return <div>
+      <Header></Header>
+      <div className='editPanel'>
+        <div ref="codeEditor" className='codeEditor'></div>
+        <div className='panel-wrap'>
+          <div className='panel_container'>
+            <div className='panel_header'>
+              <div className='avator' alt=""/>
+              <div className="userPhone">18229060809</div>
             </div>
-        </div>;
-    }
-    getFormatData(){
-        let self = this;
-        let editor = ace.edit(self.refs.codeEditor);
-        let data = editor.getValue();
-        let format = jsBeautify.js_beautify(data, { indent_size: 2 });
-        editor.setValue(format);
-    }
-    // 提交用户上传数据
-    submitData(){
-        let self = this;
-        let data = ace.edit(self.refs.codeEditor).getValue();
-        // 格式化后进行校验 校验通过进行处理
-        fetchJsonp('http://127.0.0.1:7001/submit?data='+data)
-        .then(function(response) { return response.json();})
-        .then(res => {
-            console.log(res);
-            if (res) {
-                window.location.href='http://127.0.0.1:7001/showPage';
-            }
-        });
-    }
+            <div className="dataDescribe">请按照如下格式上传数据</div>
+            <div className="dataFormat"></div>
+            <div><button className='editBtn' onClick={this.getFormatData}>格式化数据</button></div>
+            <div><button className='editBtn' onClick={this.submitData}>提交数据</button></div>
+          </div>
+        </div>
+      </div>
+    </div>;
+  }
+  getFormatData(){
+    let self = this;
+    let editor = ace.edit(self.refs.codeEditor);
+    let data = editor.getValue();
+    let format = jsBeautify.js_beautify(data, { indent_size: 2 });
+    editor.setValue(format);
+  }
+  // 提交用户上传数据
+  submitData(){
+    let self = this;
+    let data = ace.edit(self.refs.codeEditor).getValue();
+    // 格式化后进行校验 校验通过进行处理
+    fetchJsonp('http://127.0.0.1:7001/submit?data='+data)
+    .then(function(response) { return response.json();})
+    .then(res => {
+      console.log(res);
+      if (res) {
+        window.location.href='http://127.0.0.1:7001/showPage';
+      }
+    });
+  }
 }
